8e677ec566b8332fe1a2c8b79b505f11c3e45799
Abdullah801953 <132187695+Abdullah801953@users.noreply.github.com>
2025-06-17 20:14:29 +0530

    chat-final-commit

---
+++ backend/src/index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
+import {connectDB} from './lib/db.js';
+
+import authRoutes from './routes/auth.route.js';
+import messageRoute from "./routes/message.route.js"
+import {app, server} from './lib/socket.js';
+
+dotenv.config();
+
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+  origin:"http://localhost:5173",
+  credentials:true,
+}))
+
+const PORT =process.env.PORT;
+
+app.use("/api/auth",authRoutes);
+app.use("/api/messages",messageRoute)
+
+server.listen(PORT, () => {
+  console.log('Server is running on port', PORT);
+  connectDB();
+});
